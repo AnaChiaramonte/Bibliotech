1d5ccbfa6e299b85341b4e9b605b24d4d5ed90b9
PEDRO ARAUJO <mxpedro.araujo11@gmail.com>
2025-02-11 15:20:11 -0300

    Teste branches

--- Bibliotech/Bibliotech/WeatherForecast.cs
+++ Bibliotech/Bibliotech/WeatherForecast.cs
@@ -9,6 +9,6 @@ namespace Bibliotech
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        public int Teste { get; set; }
+        public int teste { get; set; }
     }
 }
